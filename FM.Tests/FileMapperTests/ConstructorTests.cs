49c58d3cefa5a9f5228ec5251f564af48a9f5a49
Petar Petrov <primas.petrov@gmail.com>
2017-03-14 12:09:07 +0200

    Added test for exception in Constructor

--- FM.Tests/FileMapperTests/ConstructorTests.cs
+++ FM.Tests/FileMapperTests/ConstructorTests.cs
@@ -21,5 +21,29 @@ namespace FM.Tests.FileMapperTests
             // Act and assert
             ThrowsAssert.Throws<ArgumentNullException>(() => new FileMapper(null, mockedLogger.Object));
         }
+
+        [TestMethod]
+        public void ShouldThrowNullArgumentWithSpecificMessage_WhenNoFileManagerIsSupplied()
+        {
+            // Arrange
+            var mockedLogger = new Mock<ILogger>();
+
+            // Act
+            var exceptionThrown = ThrowsAssert
+                .Throws<ArgumentNullException>(() => new FileMapper(null, mockedLogger.Object));
+
+            // Assert
+            StringAssert.Contains(exceptionThrown.Message.ToLower(), "filemanager is null".ToLower());
+        }
+
+        [TestMethod]
+        public void ShouldThrowNullArgument_WhenNoLoggerIsSupplied()
+        {
+            // Arrange
+            var mockedManger = new Mock<IFileManager>();
+
+            // Act and assert
+            ThrowsAssert.Throws<ArgumentNullException>(() => new FileMapper(mockedManger.Object, null));
+        }
     }
 }
