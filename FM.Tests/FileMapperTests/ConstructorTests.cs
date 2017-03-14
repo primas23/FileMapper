dd6f0ef19a0f619ac8078bbdb22e76e4b85affc3
Petar Petrov <primas.petrov@gmail.com>
2017-03-14 11:54:13 +0200

    Added testproject and test

---
+++ FM.Tests/FileMapperTests/ConstructorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSTestExtensions;
+
+using FM.Common.Contracts;
+using FM.Mapper;
+
+namespace FM.Tests.FileMapperTests
+{
+    [TestClass]
+    public class ConstructorTests
+    {
+        [TestMethod]
+        public void ShouldThrowNullArgument_WhenNoFileManagerIsSupplied()
+        {
+            // Arrange
+            var mockedLogger = new Mock<ILogger>();
+
+            // Act and assert
+            ThrowsAssert.Throws<ArgumentNullException>(() => new FileMapper(null, mockedLogger.Object));
+        }
+    }
+}
